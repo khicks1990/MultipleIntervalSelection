a7c1180a2995171fc05661058499440ff2c772de
khicks1990 <85317318+khicks1990@users.noreply.github.com>
2024-12-31 07:07:00 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.border.Border;
+
+public class Main {
+
+       /**
+          The main method creates an instance of the
+          MultipleIntervalSelection class which causes it
+          to display its window.
+       */
+
+       public static void main(String[] args)
+       {
+          new MultipleIntervalSelection();
+       }
+}
